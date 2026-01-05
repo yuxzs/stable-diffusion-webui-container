6948a8181ccc9b3fc88ff98f576de58653864d70
yuxs <yjvync9v@gmail.com>
2026-01-05 10:43:01 +0800

    Add '-f' flag to webui.sh execution

--- entrypoint.sh
+++ entrypoint.sh
@@ -29,5 +29,5 @@ git config --global --add safe.directory /home/sduser/stable-diffusion-webui
 # --api: 開啟 API
 # --xformers: 加速推論 (建議加上，雖然 webui.sh 會試著裝)
 # --data-dir: 指定資料儲存位置 (可選)
-exec ./webui.sh --listen --api --port 7860
+exec ./webui.sh -f --listen --api --port 7860
 # python launch.py -f --xformers --opt-sdp-attention --no-half-vae
